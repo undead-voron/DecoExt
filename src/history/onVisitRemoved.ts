b8cd11268d3db849662aa4d3e5e605ad6eb9388f
Den Inozemtsev <undeadvoron@gmail.com>
2025-03-13 21:13:27 +0400

    refactor history

--- src/history/onVisitRemoved.ts
+++ src/history/onVisitRemoved.ts
@@ -1,61 +1,22 @@
 import browser from 'webextension-polyfill'
 
-import { createDecorator } from '~/buildDecoratorAndMethodWrapper'
+import { buildDecoratorAndMethodWrapper } from '~/buildDecoratorAndMethodWrapper'
 import { callOnce } from '~/utils'
-import container from '../injectablesContainer'
-import { resolve } from '../instanceResolver'
 
-// Define the removed visit info type based on the browser API documentation
-interface RemovedVisits {
-  allHistory: boolean
-  urls?: string[]
-}
+type AllowedListener = ((...args: any[]) => any) | (() => unknown) | ((removed: browser.History.OnVisitRemovedRemovedType) => unknown)
 
-type AllowedListener = ((...args: any[]) => any) | (() => unknown) | ((arg: { removed: RemovedVisits }) => unknown)
-
-const listeners = new Set<(arg: { removed: RemovedVisits }) => Promise<void>>()
+const listeners = new Set<(removed: browser.History.OnVisitRemovedRemovedType) => Promise<void>>()
 
 const createInitialListener = callOnce(() => {
   browser.history.onVisitRemoved.addListener((removed) => {
     for (const listener of listeners)
-      listener({ removed })
+      listener(removed)
   })
 })
 
-const removedInfoDecoratorInfo = createDecorator<keyof RemovedVisits | void>('removedInfo')
+const { listenerWrapper, decorator } = buildDecoratorAndMethodWrapper<browser.History.OnVisitRemovedRemovedType, AllowedListener>('removedInfo')
 
-export const removedInfo = removedInfoDecoratorInfo.decorator
-
-// map onVisitRemoved data to parameter decorators
-function decoratorsHandler(arg: { removed: RemovedVisits }, constructor: any, propertyKey: string | symbol): Array<any> {
-  const existingRemovedInfoParameters: { index: number, key?: keyof RemovedVisits }[] = Reflect.getOwnMetadata(removedInfoDecoratorInfo.key, constructor, propertyKey) || []
-
-  if (existingRemovedInfoParameters.length) {
-    const customArg = []
-    for (const { index, key } of existingRemovedInfoParameters) {
-      customArg[index] = key ? arg.removed[key] : arg.removed
-    }
-    return customArg
-  }
-  else {
-    return [arg]
-  }
-}
-
-function listenerWrapper(constructor: any, method: AllowedListener, propertyKey: string | symbol) {
-  return async (arg: { removed: RemovedVisits }): Promise<void> => {
-    const instanceWrapperConstructor = container.get(constructor.constructor)
-    if (!instanceWrapperConstructor)
-      throw new Error('decorator should be applied on class decorated by "Service" decorator')
-
-    const instance = resolve(instanceWrapperConstructor)
-    if (instance.init && typeof instance.init === 'function') {
-      // await instance initialization
-      await instance.init()
-    }
-    method.call(instance, ...decoratorsHandler(arg, constructor, propertyKey))
-  }
-}
+export const removedInfo = decorator
 
 /**
  * @overview
--- src/history/onVisited.ts
+++ src/history/onVisited.ts
@@ -1,55 +1,22 @@
 import browser from 'webextension-polyfill'
 
-import { createDecorator } from '~/buildDecoratorAndMethodWrapper'
+import { buildDecoratorAndMethodWrapper } from '~/buildDecoratorAndMethodWrapper'
 import { callOnce } from '~/utils'
-import container from '../injectablesContainer'
-import { resolve } from '../instanceResolver'
 
-type AllowedListener = ((...args: any[]) => any) | (() => unknown) | ((arg: { historyItem: browser.History.HistoryItem }) => unknown)
+type AllowedListener = ((...args: any[]) => any) | (() => unknown) | ((historyItem: browser.History.HistoryItem) => unknown)
 
-const listeners = new Set<(arg: { historyItem: browser.History.HistoryItem }) => Promise<void>>()
+const listeners = new Set<(historyItem: browser.History.HistoryItem) => Promise<void>>()
 
 const createInitialListener = callOnce(() => {
   browser.history.onVisited.addListener((historyItem) => {
     for (const listener of listeners)
-      listener({ historyItem })
+      listener(historyItem)
   })
 })
 
-const historyItemDecoratorInfo = createDecorator<keyof browser.History.HistoryItem | void>('historyItem')
+const { listenerWrapper, decorator } = buildDecoratorAndMethodWrapper<browser.History.HistoryItem, AllowedListener>('historyItem')
 
-export const historyItem = historyItemDecoratorInfo.decorator
-
-// map onVisited data to parameter decorators
-function decoratorsHandler(arg: { historyItem: browser.History.HistoryItem }, constructor: any, propertyKey: string | symbol): Array<any> {
-  const existingHistoryItemParameters: { index: number, key?: keyof browser.History.HistoryItem }[] = Reflect.getOwnMetadata(historyItemDecoratorInfo.key, constructor, propertyKey) || []
-
-  if (existingHistoryItemParameters.length) {
-    const customArg = []
-    for (const { index, key } of existingHistoryItemParameters) {
-      customArg[index] = key ? arg.historyItem[key] : arg.historyItem
-    }
-    return customArg
-  }
-  else {
-    return [arg]
-  }
-}
-
-function listenerWrapper(constructor: any, method: AllowedListener, propertyKey: string | symbol) {
-  return async (arg: { historyItem: browser.History.HistoryItem }): Promise<void> => {
-    const instanceWrapperConstructor = container.get(constructor.constructor)
-    if (!instanceWrapperConstructor)
-      throw new Error('decorator should be applied on class decorated by "Service" decorator')
-
-    const instance = resolve(instanceWrapperConstructor)
-    if (instance.init && typeof instance.init === 'function') {
-      // await instance initialization
-      await instance.init()
-    }
-    method.call(instance, ...decoratorsHandler(arg, constructor, propertyKey))
-  }
-}
+export const historyItem = decorator
 
 /**
  * @overview
